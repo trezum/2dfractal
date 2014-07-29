6471fbc48d445576e9edfc2ddac9fc8910caa598
Lars Petersen <Trezum@gmail.com>
2014-07-29 15:27:08 +0200

    initial commit

---
+++ 2dFractal/2dFractal/CalculationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace _2dFractal
+{
+    public class CalculationService
+    {
+        public Color GetPointColour()
+        {
+            return Color.FromRgb(100, 100, 0);
+        }
+    }
+}
---
+++ 2dFractal/2dFractal/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Image = System.Windows.Controls.Image;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace _2dFractal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CreateAndShowMainWindow();
+        }
+
+        private void CreateAndShowMainWindow()
+        {
+            // Create the application's main window
+            //mainWindow = new Window();
+            //mainWindow.Title = "Writeable Bitmap";
+            //mainWindow.Height = 200;
+            //mainWindow.Width = 200;
+
+            // Define the Image element
+            _random.Stretch = Stretch.None;
+            _random.Margin = new Thickness(20);
+
+            // Define a StackPanel to host Controls
+            StackPanel myStackPanel = new StackPanel();
+            myStackPanel.Orientation = Orientation.Vertical;
+            myStackPanel.Height = 200;
+            myStackPanel.VerticalAlignment = VerticalAlignment.Top;
+            myStackPanel.HorizontalAlignment = HorizontalAlignment.Center;
+
+            // Add the Image to the parent StackPanel
+            myStackPanel.Children.Add(_random);
+
+            // Add the StackPanel as the Content of the Parent Window Object
+            mainWindow.Content = myStackPanel;
+            mainWindow.Show();
+
+            // DispatcherTimer setup
+            // The DispatcherTimer will be used to update _random every
+            //    second with a new random set of colors.
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.IsEnabled = true;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+        //  System.Windows.Threading.DispatcherTimer.Tick handler
+        //
+        //  Updates the Image element with new random colors
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            //Update the color array with new random colors
+            Random value = new Random();
+            value.NextBytes(_colorArray);
+
+            //Update writeable bitmap with the colorArray to the image.
+            _wb.WritePixels(_rect, _colorArray, _stride, 0);
+
+            //Set the Image source.
+            _random.Source = _wb;
+
+        }
+
+        private Image _random = new Image();
+        // Create the writeable bitmap will be used to write and update.
+        private static WriteableBitmap _wb =
+            new WriteableBitmap(100, 100, 96, 96, PixelFormats.Bgra32, null);
+        // Define the rectangle of the writeable image we will modify. 
+        // The size is that of the writeable bitmap.
+        private static Int32Rect _rect = new Int32Rect(0, 0, _wb.PixelWidth, _wb.PixelHeight);
+        // Calculate the number of bytes per pixel. 
+        private static int _bytesPerPixel = (_wb.Format.BitsPerPixel + 7) / 8;
+        // Stride is bytes per pixel times the number of pixels.
+        // Stride is the byte width of a single rectangle row.
+        private static int _stride = _wb.PixelWidth * _bytesPerPixel;
+
+        // Create a byte array for a the entire size of bitmap.
+        private static int _arraySize = _stride * _wb.PixelHeight;
+        private static byte[] _colorArray = new byte[_arraySize];
+    }
+}
